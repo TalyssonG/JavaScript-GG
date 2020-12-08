2c055222fd3617b93a1bd41ba5ae655e11a32aa5
TalyssonG <lopestalysson99@gmail.com>
2020-12-08 14:23:50 -0300

    Exercícios

---
+++ Condições/index.js
@@ -0,0 +1,11 @@
+var vel = 50.5;
+
+if (vel <= 80) {
+     console.log(`A velocidade do seu carro é ${vel}km/h`);
+     console.log(`Dirija sempre usando cinto de segurança!`);
+}else{
+  if (vel > 80) {
+     console.log(`A velocidade do seu carro é ${vel}km/h`);
+     console.log("MULTADO");
+  }
+}
