7fb9dc1b2b1affab06765c5dd39185e1ae55c633
TalyssonG <lopestalysson99@gmail.com>
2020-12-10 02:01:55 -0300

    Condições parte 2

---
+++ Condições/condições-Parte2/ex13.js
@@ -0,0 +1,15 @@
+
+var agora = new Date()
+var hora = agora.getHours()
+
+console.log(`Agora são exatamente ${hora} horas`)
+
+if(hora > 5 && hora < 12){
+    console.log('Bom dia')
+}else if (hora > 12 && hora < 17){
+    console.log('Boa tarde')
+}else if(hora > 17 && hora < 23){
+    console.log('Boa noite')
+}else if(hora => 23 && hora < 5){
+    console.log('Boa madrugada')
+}
