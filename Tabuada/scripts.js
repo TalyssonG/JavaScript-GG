d96aac12d24d00d0b6116d533c86d960e32bcbc8
TalyssonG <lopestalysson99@gmail.com>
2020-12-17 16:04:37 -0300

    Tabuada

---
+++ Tabuada/scripts.js
@@ -0,0 +1,25 @@
+
+
+var tab = document.getElementById('seltab')
+
+function tabuada() {
+    let num = document.getElementById('txtn')
+    if (num.value.length == 0) {
+        alert('Digite um número')
+    } else {
+        let n = Number(num.value)
+        let c = 1
+        tab.innerHTML = ''
+        while(c <=10) {
+            let item = document.createElement('option')
+            item.text = `${n} x ${c} = ${n*c}`
+            item.value = `tab${c}` // Importante para o PHP
+            tab.appendChild(item)
+            c++
+        }
+    }
+}
+
+function limpar() {
+    tab.innerHTML = '<option>Digite outro valor</option>'
+}
